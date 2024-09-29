53ffe2e11110e506f19f891db3f88633a930c2fa
devjeetroy98 <devjeetroy.dr@gmail.com>
2024-09-29 22:51:55 +0530

    Short Polling implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [events, setEvents] = useState([] as any[]);
+  const [eventName, setEventName] = useState("");
+  const baseURL = "http://localhost:8080";
+
+  useEffect(() => {
+    let timer = setInterval(() => fetchEvents(), 10000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  function handleChange(e: any) {
+    setEventName(e.target.value);
+  }
+
+  async function handleSubmit() {
+    let response: any = await axios.post(`${baseURL}/rest/create`, {
+      eventName,
+      isEventRegistered: false,
+    });
+    if (response.data && response.data._id) {
+      setEventName("");
+      setEvents((prevState: any) => [...prevState, response.data]);
+    }
+  }
+
+  async function fetchEvents() {
+    let fetchedEvents: any = await axios.get(`${baseURL}/rest/findAll`);
+    setEvents(fetchedEvents.data);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container-fluid">
+      <div className="form-container">
+        <div className="form-control">
+          <label htmlFor="event">Event Name</label>
+          <input
+            type="text"
+            name="event"
+            id="event"
+            placeholder="Enter the event name"
+            value={eventName}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="form-control">
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="event-container">
+        <div className="events">
+          {events.map((event: any) => {
+            console.log(event);
+            return (
+              <div className="event-card" key={event._id}>
+                {event.isEventRegistered ? (
+                  <div className="tick">
+                    <div>&#x2713;</div>
+                  </div>
+                ) : (
+                  <div>
+                    <img src="/loader.gif" alt="" />
+                  </div>
+                )}
+                <div className="event-title">{event.eventName}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
